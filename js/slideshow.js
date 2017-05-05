9267efe149a2525774a978599ad4544581b3b43f
Daniel (Woo Jin) Lee <daniul.lee@gmail.com>
2017-05-05 15:16:02 -0400

    Revert "final"
    This reverts commit b81eeb58a01257fd13e875a841029789637fcde9.

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 $("#slideshow > div:gt(0)").hide();
 
 setInterval(function() {
@@ -8,3 +10,6 @@ setInterval(function() {
     .end()
     .appendTo('#slideshow');
 },  3000);
+#
+
+});
